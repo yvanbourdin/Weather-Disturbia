814d14d5d105b064ebedf940a1a2df280ec61f10
Yvan Bourdin <yvankiller92@gmail.com>
2021-12-24 00:38:41 +0100

    Add enemy & healthbar

---
+++ Weather Disturbia/Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    public SpriteRenderer graphics;
+    private Transform target;
+    private int destPoint = 0;
+
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        // If the enemy is almost arrived to its destination
+        if(Vector3.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
---
+++ Weather Disturbia/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public Gradient gradient;
+    public Image fill;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+
+        // Set the color of healthbar at 1 on the gradient
+        fill.color = gradient.Evaluate(1f);
+    }
+
+    public void SetHealth(int _health)
+    {
+        slider.value = _health;
+        fill.color = gradient.Evaluate(slider.normalizedValue); // Get the value between 0 and 1 because this is the limits of the gradient values
+    }
+}
