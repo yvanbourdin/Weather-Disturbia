618140fc3645db1fe5cec5990b5bf3147f2ae926
Yvan Bourdin <yvankiller92@gmail.com>
2021-12-24 14:09:05 +0100

    Add inventory, fix jump & refactoring

---
+++ Weather Disturbia/Assets/Scripts/Inventory.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public int coinsCount;
+    public Text coinsCountText;
+
+    public static Inventory instance;
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Debug.LogWarning("There is more than one instance of Inventory in the scene");
+            return;
+        }
+        instance = this;
+    }
+
+    public void AddCoins(int _count)
+    {
+        coinsCount += _count;
+        coinsCountText.text = coinsCount.ToString();
+    }
+}
