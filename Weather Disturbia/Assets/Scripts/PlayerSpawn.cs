88090851eb3e0d01009add29fdc6ca82cac89ae1
Yvan Bourdin <yvankiller92@gmail.com>
2021-12-24 16:56:23 +0100

    Add another level & death zones

---
+++ Weather Disturbia/Assets/Scripts/PlayerSpawn.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    private void Awake()
+    {
+        // Place the player at the position of this object (PlayerSpawn)
+        GameObject.FindGameObjectWithTag("Player").transform.position = transform.position;
+    }
+}
