e2bcfa0fbd816cc21866afffc1719b18883919ea
Yvan Bourdin <yvankiller92@gmail.com>
2021-12-24 01:31:21 +0100

    Add enemy damages

--- Weather Disturbia/Assets/Scripts/PlayerHealth.cs
+++ Weather Disturbia/Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
     public int currentHealth;
 
+    public float invicibilityTimeAfterHit = 3f;
+    public float invicibilityFlashDelay = 0.15f;
+    public bool isInvicible = false;
+
+    public SpriteRenderer graphics;
     public HealthBar healthBar;
 
     void Start()
@@ -21,9 +27,32 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    void TakeDamage(int damage)
+    public void TakeDamage(int damage)
     {
-        currentHealth -= damage;
-        healthBar.SetHealth(currentHealth);
+        if(!isInvicible)
+        {
+            currentHealth -= damage;
+            healthBar.SetHealth(currentHealth);
+            isInvicible = true;
+            StartCoroutine(InvicibilityFlash());
+            StartCoroutine(HandleInvicibilityDelay());
+        }
+    }
+
+    public IEnumerator InvicibilityFlash()
+    {
+        while(isInvicible)
+        {
+            graphics.color = new Color(1f, 1f, 1f, 0f);
+            yield return new WaitForSeconds(invicibilityFlashDelay);
+            graphics.color = new Color(1f, 1f, 1f, 1f);
+            yield return new WaitForSeconds(invicibilityFlashDelay);
+        }
+    }
+
+    public IEnumerator HandleInvicibilityDelay()
+    {
+        yield return new WaitForSeconds(invicibilityTimeAfterHit);
+        isInvicible = false;
     }
 }
