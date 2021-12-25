1bb56bf08fad9c6fc6033dadaed921613277d3ac
Yvan Bourdin <yvankiller92@gmail.com>
2021-12-25 16:07:58 +0100

    Add checkpoints & ladder system

---
+++ Weather Disturbia/Assets/Scripts/Ladder.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ladder : MonoBehaviour
+{
+    public bool isInRange;
+    private PlayerMovement playerMovement;
+    public BoxCollider2D topCollider;
+    public Text interactUI;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        playerMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isInRange && playerMovement.isClimbing && Input.GetKeyDown(KeyCode.E))
+        {
+            playerMovement.isClimbing = false;
+            topCollider.isTrigger = false;
+            return;
+        }
+
+        if(isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            playerMovement.isClimbing = true;
+            topCollider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            playerMovement.isClimbing = false;
+            topCollider.isTrigger = false;
+            interactUI.enabled = false;
+        }
+    }
+}
